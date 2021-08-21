c6b2a3cf03a4d8be7325f2f0536fe56315d9574d
arshedgithub <arshedahmedmail@gmail.com>
2021-08-21 07:15:08 +0530

    getting the current user

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,13 @@ const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -17,7 +22,11 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ["_id", "name", "email"]));
+  const token = user.generateAuthToken();
+
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
