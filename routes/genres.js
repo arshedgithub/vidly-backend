6dd60bf5e9df34f10adfbca7bc2ca7cb371d5b1c
arshedgithub <arshedahmedmail@gmail.com>
2021-08-22 08:47:31 +0530

    removing try catch

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require("../middleware/async");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
@@ -5,24 +6,27 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
     res.send(genres);
-  } catch (ex) {
-    next(ex);
-  }
-});
+  })
+);
 
-router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
 
-  res.send(genre);
-});
+    res.send(genre);
+  })
+);
 
 router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
