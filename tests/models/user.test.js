d81edcf11e8ec3de6b9d7dae762f496ece129c1a
arshedgithub <arshedahmedmail@gmail.com>
2021-08-23 13:55:14 +0530

    wrote a unit test

---
+++ tests/models/user.test.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const { User } = require("../../models/user");
+const config = require("config");
+
+describe("user.generateAuthToken", () => {
+  it("should return a valid JWT token", () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(payload);
+  });
+});
